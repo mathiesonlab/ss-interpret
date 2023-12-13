caf72fd44bc3edff5986f66c4d4947ee90fa791c
Sara Mathieson <sara.k.mathieson@gmail.com>
2023-12-13 10:43:54 -0500

    fix

--- hidden.py
+++ hidden.py
@@ -201,8 +201,7 @@ if __name__ == "__main__":
                     disc_along_genome(iterator, input_file, output_file)'''
 
         # fine tuning
-        saved_model = disc_folders[0][:3] + "_" + str(i) + "_" + date + "_finetune"
-        print(saved_model)
+        saved_model = disc_folders[0][:3] + "_" + str(i) + "_" + date + "_finetuneAug23"
         if saved_model in disc_folders: # already trained
             input_file = input_folder + saved_model
             print("input disc", input_file)
