6abb3fec2d27fea871231e40fe5c6e503436b019
Sara Mathieson <sara.k.mathieson@gmail.com>
2023-12-13 10:54:52 -0500

    heatmap

--- scripts/heatmaps.sh
+++ scripts/heatmaps.sh
@@ -3,11 +3,11 @@ TEST_POP=${2} # second command line arg (i.e. GBR)
 
 MAIN_FOLDER="/Users/smathieson/Dropbox/ss-interpret"
 #DISC_FOLDER="/homes/smathieson/Documents/pg_gan_interpret/discriminators_og"
-DATE="230410"
+DATE="230410_230830"
 
 for SEED in $(seq 0 19)
 do
-  DISC=${TRAIN_POP}"_"${SEED}"_"${DATE}"_finetune"
+  DISC=${TRAIN_POP}"_"${SEED}"_"${DATE}"_finetuneAug23"
   
   # HEATMAPS (all stats)
   #echo "python3 correlation_heatmap.py ${MAIN_FOLDER}/summary_stats/stats_${TEST_POP}.npy ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps/afterperm_${DISC}_${TEST_POP}.pdf"
@@ -15,5 +15,5 @@ do
 
   # HEATMAPS (per-SNP pi)
   echo "python3 correlation_heatmap.py ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps/perSnpPi_${DISC}_${TEST_POP}.pdf"
-  python3 correlation_heatmap.py ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps/perSnpPi_${DISC}_${TEST_POP}.pdf
+  #python3 correlation_heatmap.py ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps/perSnpPi_${DISC}_${TEST_POP}.pdf
 done
