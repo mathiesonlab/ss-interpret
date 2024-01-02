9c73bd4676cb74dcc1e6f8f1f0eb468f191a7f48
Sara Mathieson <sara.k.mathieson@gmail.com>
2024-01-02 16:26:46 -0500

    toggle

--- scripts/heatmaps.sh
+++ scripts/heatmaps.sh
@@ -3,17 +3,17 @@ TEST_POP=${2} # second command line arg (i.e. GBR)
 
 MAIN_FOLDER="/Users/smathieson/Dropbox/ss-interpret"
 #DISC_FOLDER="/homes/smathieson/Documents/pg_gan_interpret/discriminators_og"
-DATE="230410"
+DATE="230410_230830"
 
 for SEED in $(seq 0 19)
 do
-  DISC=${TRAIN_POP}"_"${SEED}"_"${DATE}"_finetune"
+  DISC=${TRAIN_POP}"_"${SEED}"_"${DATE}"_finetuneAug23"
   
   # HEATMAPS (all stats)
-  #echo "python3 correlation_heatmap.py ${MAIN_FOLDER}/summary_stats/stats_${TEST_POP}.npy ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps/afterperm_${DISC}_${TEST_POP}.pdf"
-  #python3 correlation_heatmap.py ${MAIN_FOLDER}/summary_stats/stats_${TEST_POP}.npy ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps/afterperm_${DISC}_${TEST_POP}.pdf
+  #echo "python3 correlation_heatmap.py ${MAIN_FOLDER}/summary_stats/stats_${TEST_POP}.npy ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps_Aug23/afterperm_${DISC}_${TEST_POP}.pdf"
+  #python3 correlation_heatmap.py ${MAIN_FOLDER}/summary_stats/stats_${TEST_POP}.npy ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps_Aug23/afterperm_${DISC}_${TEST_POP}.pdf
 
   # HEATMAPS (per-SNP pi)
-  echo "python3 correlation_heatmap.py ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps/perSnpPi_${DISC}_${TEST_POP}.pdf"
-  python3 correlation_heatmap.py ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps/perSnpPi_${DISC}_${TEST_POP}.pdf
+  echo "python3 correlation_heatmap.py ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps_Aug23/perSnpPi_${DISC}_${TEST_POP}.pdf"
+  python3 correlation_heatmap.py ${MAIN_FOLDER}/hidden_pi/hidden_pi_${DISC}_${TEST_POP}.txt ${MAIN_FOLDER}/heatmaps_Aug23/perSnpPi_${DISC}_${TEST_POP}.pdf
 done
