7f140439050988d97d91960f87cd8cafea30a930
Sara Mathieson <sara.k.mathieson@gmail.com>
2024-02-14 11:27:48 -0500

    read weights

--- read_saved_model.py
+++ read_saved_model.py
@@ -1,7 +1,13 @@
 
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+#from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
 
+from correlation_heatmap import parse_correlation_file
+
+hidden_pi_file = "/Users/smathieson/Dropbox/ss-interpret/hidden_pi/hidden_pi_CEU_0_230410_finetune_GBR.txt"
+all_hidden, common_indices = parse_correlation_file(hidden_pi_file)
+print(common_indices)
 path = "/Users/smathieson/Dropbox/ss-interpret/trained_discs/CEU/CEU_0_230410_finetune/"
 variables = "variables/variables" #.data-00001-of-00002"
 filename = path + variables
@@ -16,10 +22,17 @@ filename = path + variables
 #reader = pywrap_tensorflow.NewCheckpointReader(filename)
 reader = tf.train.load_checkpoint(filename)
 var_to_shape_map = reader.get_variable_to_shape_map()
+fig1 = plt.figure()
+num_rows = len(common_indices)
+num_cols = 1
 for key in sorted(var_to_shape_map):
     if key.startswith("conv2/kernel"):
         print("tensor_name: ", key)
         weights = reader.get_tensor(key)
         print(weights.shape)
-        print(weights[:,:,:,10])
-        input('enter')
+        for (i, idx) in enumerate(common_indices):
+            filter = weights[0,:,:,idx]
+            print(filter.shape)
+            fig1 = plt.subplot(num_rows, num_cols, i+1)
+            plt.imshow(filter, cmap='gray')
+plt.show()
