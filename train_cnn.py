b13c0a5a889a763354f5c844261fafc7a6cda281
Sara Mathieson <smathieson@haverford.edu>
2024-06-07 15:29:55 -0400

    cnn train

--- train_cnn.py
+++ train_cnn.py
@@ -7,8 +7,7 @@ Date: 8/21/23
 """
 
 # python imports
-#import math
-#import matplotlib.pyplot as plt
+import keras
 import numpy as np
 import random
 import sys
@@ -16,10 +15,8 @@ import tensorflow as tf
 
 # our imports
 import discriminator
-#import genome_disc
 import global_vars
 from slim_iterator import SlimIterator
-#import util_evals
 
 ################################################################################
 # GLOBALS
@@ -29,24 +26,14 @@ from slim_iterator import SlimIterator
 #MAIN_PATH = "/homes/smathieson/Documents/pg_gan_interpret/"
 
 # globals
-TRAIN_POP = sys.argv[1] # ALL for ALL_AI
-SEL_TYPE = sys.argv[2] # change for different types of selection (i.e. Aug23, Over)
+TRAIN_POP = sys.argv[1] # i.e. CEU, ALL for ALL_AI
+SEL_TYPE = sys.argv[2]  # change for different types of selection (i.e. Aug23, Over)
 
-#TEST_H5 = "/bigdata/smathieson/1000g-share/HDF5/" + TEST_POP + ".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.h5"
-#BED = "/bigdata/smathieson/1000g-share/HDF5/20120824_strict_mask.bed"
-#SEL_EXCEL = MAIN_PATH + "selected_regions/selected_regions.xlsx"
 SLIM_DATA = "/bigdata/smathieson/pg-gan/1000g/SLiM/Aug23/" + TRAIN_POP + "_" + SEL_TYPE + "/"
-
-#NONTRAIN_PATH = MAIN_PATH + "discriminators_og/nontrain"
-#TRAIN_PATH = MAIN_PATH + "discriminators_og/" + TRAIN_POP
-#PRED_PREFIX = MAIN_PATH + "discriminators_og/predictions/prob_"
 
 NEUTRAL = "neutral"
 SELECTION = ["sel_01", "sel_025", "sel_05", "sel_10"]
-#if SEL_TYPE == "AI":
-#    SELECTION = ["selection"]
-#    #SAMPLE_SIZES = [216,198,4]
-NUM_BATCH = 2000
+BATCH_PER_EPOCH = 200 # arbitrary
 
 ################################################################################
 # HELPERS
@@ -62,134 +49,83 @@ def accuracy(y_true, y_pred): # y_pred is probabilities, use 0.5 threshold
             a += 1
     return a/len(y_true)
 
-def get_mixed_batch(batch_size, neutral_iterator, sel_iterators):
-    # get a batch of half neutral data and half simulated data
+class SlimSequence(keras.utils.Sequence):
 
-    # not actually "real" data
-    half = batch_size//2
-    neutral_regions = neutral_iterator.real_batch(batch_size=half)
-    sel_regions = mixed_sel_batch(sel_iterators, half)
-    regions = np.concatenate((neutral_regions, sel_regions), axis=0)
-    labels = np.concatenate((np.zeros((half,1)), np.ones((half,1))))
+    def __init__(self, neutral_iterator, sel_iterators, train, batch_size=None):
+        self.neutral_iterator = neutral_iterator
+        self.sel_iterators = sel_iterators
+        self.batch_size = batch_size # not needed for test
+        self.train = train # boolean
 
-    # shuffle
-    idx = np.random.permutation(batch_size)
-    regions, labels = regions[idx], labels[idx]
-   
-    return regions, labels
+    def __len__(self):
+        """ number of batches per epoch """
+        if self.train:
+            return BATCH_PER_EPOCH
+        return 1
 
-def mixed_sel_batch(sel_iterators, num_regions):
-    # num_regions should be half a batch normally
-    x = len(sel_iterators) # num iters
-    mini = num_regions//x
-    excess = num_regions - mini*x 
-    sel_regions = [iter.real_batch(batch_size=mini) for iter in sel_iterators]
-    excess_regions = random.choice(sel_iterators).real_batch(batch_size=excess)
-    sel_regions = np.concatenate(sel_regions + [excess_regions], axis=0)
+    def __getitem__(self, idx):
+        if self.train:
+            # not actually "real" data
+            half = self.batch_size//2
+            neutral_regions = self.neutral_iterator.real_batch(batch_size=half)
+            sel_regions = self.mixed_sel_batch(half)
+            regions = np.concatenate((neutral_regions, sel_regions), axis=0)
+            labels = np.concatenate((np.zeros((half,1)), np.ones((half,1))))
 
-    assert len(sel_regions) == num_regions
-    return sel_regions
+            # shuffle
+            idx = np.random.permutation(self.batch_size)
+            regions, labels = regions[idx], labels[idx]
+
+        else:
+            neutral_regions = self.neutral_iterator.test_batch()
+            num_neutral = len(neutral_regions)
+            sel_regions = [iter.test_batch() for iter in self.sel_iterators]
+            num_sel = sum([len(x) for x in sel_regions])
+
+            regions = np.concatenate([neutral_regions] + sel_regions, axis=0)
+            labels = np.concatenate((np.zeros((num_neutral,1)), np.ones((num_sel,1))))
+            # don't need to shuffle
     
-def get_test_batch(neutral_iterator, sel_iterators):
+        return regions, labels
 
-    neutral_regions = neutral_iterator.test_batch()
-    num_neutral = len(neutral_regions)
-    sel_regions = [iter.test_batch() for iter in sel_iterators]
-    num_sel = sum([len(x) for x in sel_regions])
+    def mixed_sel_batch(self, num_regions):
+        # just get selected regions
+        x = len(self.sel_iterators) # num iters
+        mini = num_regions//x
+        excess = num_regions - mini*x 
+        sel_regions = [iter.real_batch(batch_size=mini) for iter in self.sel_iterators]
+        excess_regions = random.choice(self.sel_iterators).real_batch(batch_size=excess)
+        sel_regions = np.concatenate(sel_regions + [excess_regions], axis=0)
 
-    regions = np.concatenate([neutral_regions] + sel_regions, axis=0)
-    labels = np.concatenate((np.zeros((num_neutral,1)), np.ones((num_sel,1))))
-
-    # don't need to shuffle
-    return regions, labels
+        assert len(sel_regions) == num_regions
+        return sel_regions
 
 ################################################################################
-# FINE-TUNING
+# TRAINING
 ################################################################################
 
-def fine_tune(): #, loss_filename):#, output_filename=None):
+def train(): #, loss_filename):#, output_filename=None):
     
     # SLiM data
     neutral_iterator = SlimIterator(SLIM_DATA + NEUTRAL)
     sel_iterators = [SlimIterator(SLIM_DATA + sel) for sel in SELECTION]
 
-    # pg-gan trained discriminator
-    #disc = tf.saved_model.load(disc_filename)
+    # set up CNN model
     print("num haps", neutral_iterator.num_samples)
-    input('enter')
-    disc = discriminator.OnePopModel(neutral_iterator.num_samples)
+    model = discriminator.OnePopModel(neutral_iterator.num_samples)
 
     # training params
     cross_entropy =tf.keras.losses.BinaryCrossentropy(from_logits=True)
     optimizer = tf.keras.optimizers.Adam()
 
-    # fixed test data
-    test_regions, test_labels = get_test_batch(neutral_iterator, sel_iterators)
-    print("test shape", test_regions.shape)
+    # train and validation data
+    training_generator = SlimSequence(neutral_iterator, sel_iterators, True, batch_size=global_vars.BATCH_SIZE)
+    validation_generator = SlimSequence(neutral_iterator, sel_iterators, False)
 
-    batch_lst = []
-    train_loss_lst = []
-    test_loss_lst = []
-    train_acc_lst = []
-    test_acc_lst = []
+    model.compile(optimizer=optimizer, loss=cross_entropy, metrics=['accuracy'])
 
-    i = 0
-    #stop = False
-    while i < NUM_BATCH: # and not stop:
-        with tf.GradientTape() as disc_tape:
-            
-            # get a mini-batch and make predictions
-            train_regions, train_labels = get_mixed_batch(global_vars.BATCH_SIZE, neutral_iterator, sel_iterators)
-            train_predictions = disc(train_regions, training=True)
-
-            # evaluate loss
-            train_loss = cross_entropy(train_labels, train_predictions)
-            if (i % 10) == 0:
-                print(i, "loss", train_loss.numpy())
-                test_predictions = disc(test_regions, training=False)
-                test_loss = cross_entropy(test_labels, test_predictions)
-                
-                # loss plot
-                train_acc = accuracy(train_labels, train_predictions)
-                test_acc = accuracy(test_labels, test_predictions)
-                print("train acc", train_acc, "test acc", test_acc)
-                
-                # append to lists
-                batch_lst.append(i)
-                train_loss_lst.append(train_loss.numpy())
-                test_loss_lst.append(test_loss.numpy())
-                train_acc_lst.append(train_acc)
-                test_acc_lst.append(test_acc)
-                #stop = acc >= 0.95 # stop when we hit this 
-
-            # gradient descent
-            gradients_of_discriminator = disc_tape.gradient(train_loss,
-                disc.trainable_variables)
-            optimizer.apply_gradients(zip(gradients_of_discriminator,
-                disc.trainable_variables))
-        
-        i += 1
-
-    # save after fine-tuning
-    #iterator = genome_disc.get_iterator(TEST_H5, BED)
-    #disc_finetune = discriminator.ThreePopModel(SAMPLE_SIZES[0], SAMPLE_SIZES[1],
-    #    SAMPLE_SIZES[2], saved_model=disc)
-    #disc_finetune(iterator.real_batch(batch_size=1), training=False) # just to set input shapes
-    
-    #disc.save(disc_filename + "_finetune" + SEL_TYPE)
-
-    # plot loss
-    '''plt.clf()
-    plt.plot(batch_lst, train_loss_lst, 'r--', label="train loss")
-    plt.plot(batch_lst, test_loss_lst, 'r', label="test loss")
-    plt.plot(batch_lst, train_acc_lst, 'b--', label="train acc")
-    plt.plot(batch_lst, test_acc_lst, 'b', label="test acc")
-    plt.xlabel("num batches")
-    plt.legend()
-    plt.savefig(loss_filename)'''
-
-    print("num_batches = ", batch_lst)
-    print("train_loss = ", train_loss_lst)
+    model.fit_generator(generator=training_generator,
+                        validation_data=validation_generator, epochs=10)
 
 ################################################################################
 # MAIN
@@ -197,4 +133,4 @@ def fine_tune(): #, loss_filename):#, output_filename=None):
 
 if __name__ == "__main__":
 
-    fine_tune()
+    train()
