4f81d03f1d993c1d209102c5a6eeb3fc849cb3cf
Sara Mathieson <sara.k.mathieson@gmail.com>
2024-06-26 16:05:07 -0400

    fix

--- util_evals.py
+++ util_evals.py
@@ -27,9 +27,9 @@ def confusion_matrix(true, pred, prob_thresh, real=False):
         #    start_checking = True
         #if start_checking:
 
-        if true_label == 1: # true selected region
-            print(sigmoid(pred[i]))
-            input('enter')
+        #if true_label == 1: # true selected region
+        #    print(sigmoid(pred[i]))
+        #    input('enter')
             
         conf_mat[true_label, pred_label] += 1
 
