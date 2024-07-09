467cc66a053d6ed3e130af950bf0fc545dadb407
Sara Mathieson <sara.k.mathieson@gmail.com>
2024-07-09 14:00:00 -0600

    runtime

---
+++ test_runtime.py
@@ -0,0 +1,42 @@
+# python imports
+#import tensorflow as tf
+import time
+import numpy as np
+
+# our imports
+#import param_set
+#from generator import Generator
+#import simulation
+#import global_vars
+import ss_helpers
+
+disc_before = "/Users/smathieson/Dropbox/ss-interpret/trained_discs/CEU/CEU_0_230410"
+disc_after = "/Users/smathieson/Dropbox/ss-interpret/trained_discs/CEU/CEU_0_230410_230830_finetuneAug23"
+
+# simulate
+print("sim exp")
+exp_params = param_set.ParamSet(simulation.exp)
+generator = Generator(simulation.exp, ["N1", "T1"], [198],
+                        global_vars.DEFAULT_SEED)
+generator.update_params([exp_params.N1.value, exp_params.T1.value])
+mini_batch = generator.simulate_batch(1000)
+print("x", mini_batch.shape)
+#np.save("test_batch.npy", mini_batch)
+
+# get predictions
+'''disc = tf.saved_model.load(disc_before)
+start = time.time()
+preds = disc(mini_batch)
+end = time.time()
+print("time in ms", end-start)'''
+
+# get stats
+mini_batch = np.load("test_batch.npy")
+start = time.time()
+stats = ss_helpers.stats_all(mini_batch)
+end = time.time()
+print("time in ms", end-start)
+
+
+
+    
