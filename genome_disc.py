a77fc2fd265882529c1ebacee2e9284e33785466
Sara Mathieson <smathieson@smathieson-24.local>
2025-06-23 16:10:20 -0400

    fix

--- genome_disc.py
+++ genome_disc.py
@@ -18,7 +18,7 @@ import global_vars
 import real_data_random
 
 # globals
-NUM_REGIONS = 1000
+#NUM_REGIONS = 1000
 #SEL_TYPE = "AI" # change for different types of selection (i.e. Aug23, Over1, Over2, AI)
 NUM_SNPS = global_vars.NUM_SNPS
 HIDDEN = False # if True, compute last hidden layer, o.w. compute probability
@@ -64,7 +64,7 @@ def disc_along_genome(iterator, input_folder, output_file, fine_tune_disc=None):
     # go through entire genome
     final_end = iterator.num_snps-NUM_SNPS
     num_total = 0
-    final_end = NUM_REGIONS*NUM_SNPS # fewer for testing
+    #final_end = NUM_REGIONS*NUM_SNPS # fewer for testing
     for start_idx in range(0, final_end, NUM_SNPS):
         curr_chrom = iterator.chrom_all[start_idx]
         if curr_chrom != prev_chrom:
