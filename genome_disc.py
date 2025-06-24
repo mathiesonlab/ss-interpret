b27fa80788356ebee3cf25ec830ea7b6579eaabd
Sara Mathieson <smathieson@smathieson-24.local>
2025-06-24 15:32:37 -0400

    fix

--- genome_disc.py
+++ genome_disc.py
@@ -46,7 +46,7 @@ def disc_along_genome(iterator, input_folder, output_file, fine_tune_disc=None):
         # new way of loading for later versions of tensorflow
         #disc = tf.keras.layers.TFSMLayer(input_folder, call_endpoint='serving_default')
         # for .keras models
-        disc = tf.keras.models.load_model(input_folder, custom_objects={"OnePopModel": discriminator.OnePopModel(216)}) # input_folder is a file in this case
+        disc = tf.keras.models.load_model(input_folder, custom_objects={"OnePopModel": discriminator.OnePopModel, "pop": 200}) # input_folder is a file in this case
     else:
         disc = fine_tune_disc
 
