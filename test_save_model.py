22435821a67bb5f2e4a0bd5fca8af8a84bb4e5e1
Sara Mathieson <sara.k.mathieson@gmail.com>
2025-06-25 14:13:05 -0400

    fix

--- test_save_model.py
+++ test_save_model.py
@@ -22,3 +22,32 @@ new_pop = np.random.rand(1, 40, 36, 2)#, dtype=np.float32)
 #corrected[0] = region
 pred_after = disc(new_pop, training=True)
 print("pred_after", pred_after)
+disc_optimizer = tf.keras.optimizers.Adam()
+
+def discriminator_loss(real_output, fake_output):
+
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+
+    return total_loss
+
+
+# fake training
+with tf.GradientTape() as disc_tape:
+    # use current params
+    real_regions = np.random.rand(1, 40, 36, 2)#, dtype=np.float32)
+    generated_regions = np.random.rand(1, 40, 36, 2)#, dtype=np.float32)
+
+    real_output = disc(real_regions, training=True)
+    fake_output = disc(generated_regions, training=True)
+
+    disc_loss, real_acc, fake_acc = discriminator_loss(real_output, fake_output)
+
+    # gradient descent
+    gradients_of_discriminator = disc_tape.gradient(disc_loss,
+        disc.trainable_variables)
+    disc_optimizer.apply_gradients(zip(gradients_of_discriminator,
+        disc.trainable_variables))
