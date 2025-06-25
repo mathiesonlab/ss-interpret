6957102e047f0503120a1732a5922a2a429979c9
Sara Mathieson <smathieson@smathieson-24.local>
2025-06-25 06:11:30 -0400

    fix

--- genome_disc.py
+++ genome_disc.py
@@ -147,7 +147,8 @@ if __name__ == "__main__":
     #disc_folders = ["brooks9_exp_YRI", "hall7_exp_YRI", "sammet5_exp_YRI", "goto6_exp_YRI", "hawes8_exp_YRI"]
     #for saved_model in disc_folders:
     #for i in range(20): # TODO change for some YRI
-    for i in [0] + list(range(8,20)):
+    #for i in [0] + list(range(8,20)):
+    for in in [20]:
         #print(disc_folders)
         if not HIDDEN: # only do hidden for fine-tune
             saved_model = disc_folders[0][:3] + "_" + str(i) + "_" + date + ".keras" # TODO toggle .keras
