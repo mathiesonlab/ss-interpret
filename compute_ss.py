33ffe1a346b802df3a72340f1def13106893118c
Sara Mathieson <sara.k.mathieson@gmail.com>
2025-11-18 14:51:35 -0500

    modify for mosquito data

--- compute_ss.py
+++ compute_ss.py
@@ -87,7 +87,7 @@ def compute_stats_for_dataset(samples, max_samples=None, benchmark=True):
     n_samples = len(samples)
     final_stats = []
     for i in range(n_samples):
-        final_stats.append(aggregated_stats[0,i,:] + aggregated_stats[1,i,:] + fst[i])
+        final_stats.append(np.concatenate(aggregated_stats[0,i,:], aggregated_stats[1,i,:], fst[i]))
     final_stats = np.array(final_stats)
     print("final stats", final_stats.shape)
     return final_stats
