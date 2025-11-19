1f115a058f22746a72dd33a66d6b03b38282a665
Sara Mathieson <sara.k.mathieson@gmail.com>
2025-11-19 14:02:32 -0500

    modify for mosquito data

--- compare_discs.py
+++ compare_discs.py
@@ -10,84 +10,78 @@ from tqdm import tqdm
 
 #from pg_gan.discriminator import OnePopModel
 from dataset import load_data, load_metadata
-from utils import BATCH_SIZE, apply_seed_to_path, get_model, get_model_preds_lf, iterate_seeds
+from utils import BATCH_SIZE, apply_seed_to_path, get_model, get_model_preds_lf, iterate_seeds, PREFIX
 from sklearn.metrics import classification_report, accuracy_score
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-pops = ["CEU", "CHB", "YRI"]
-model_strs = ["discs/{pop}/disc_N.keras", "models/{pop}/{pop}_N_230410.h5"]
-fc_sizes = [64, 128]
+model_str = PREFIX + "discs/disc_0.keras"
 
 def main_preds():
-    for pop in pops:
-        print(f"Computing for {pop}")
-        # load data
-        _, labels = load_data(pop)
-        metadata = load_metadata(pop)
+    #for pop in pops:
+    #print(f"Computing for {pop}")
+    # load data
+    _, labels = load_data()
+    metadata = load_metadata()
 
-        # compute accuracy for each disc on each generator
-        metadata["source"] = metadata["source"].apply(lambda x: str(x).rsplit("_", maxsplit=1)[0])
-        accs = {}
-        for model_str, fc_size in zip(model_strs, fc_sizes):
-            model_str = model_str.format(pop=pop)
-            for seed in iterate_seeds(model_str):
-                accs[seed] = {"overall": 0}
-                model_path = os.path.basename(apply_seed_to_path(model_str, seed).split(".")[0])
-                preds, _ = get_model_preds_lf(pop, model_path)
-                for generator in metadata["source"].unique():
-                    gen_labels = labels[metadata["source"] == generator]
-                    gen_preds = preds[metadata["source"] == generator] > 0.5
+    # compute accuracy for each disc on each generator
+    metadata["source"] = metadata["source"].apply(lambda x: str(x).rsplit("_", maxsplit=1)[0])
+    accs = {}
+    #for model_str, fc_size in zip(model_strs, fc_sizes):
+    seed = 0
+    accs[seed] = {"overall": 0}
+    model_path = os.path.basename(apply_seed_to_path(model_str, seed).split(".")[0])
+    preds, _ = get_model_preds_lf(pop, model_path)
+    #for generator in metadata["source"].unique():
+    gen_labels = labels[metadata["source"] == generator]
+    gen_preds = preds[metadata["source"] == generator] > 0.5
 
-                    # store accuracy
-                    acc = accuracy_score(gen_labels, gen_preds)
-                    accs[seed][generator.split("_")[1]] = acc
+    # store accuracy
+    acc = accuracy_score(gen_labels, gen_preds)
+    accs[seed][generator.split("_")[1]] = acc
 
-                    accs[seed]["overall"] += acc * (gen_preds.shape[0] / preds.shape[0])
+    accs[seed]["overall"] += acc * (gen_preds.shape[0] / preds.shape[0])
 
-            # metadata
-            model_prefix = model_str.split("N")[0].rsplit("/")[0].replace("-", "").replace("_", "")
+    # metadata
+    model_prefix = model_str.split("N")[0].rsplit("/")[0].replace("-", "").replace("_", "")
 
-            # save
-            accs_df = pd.DataFrame(accs).T  # rows=seeds, cols=generators
-            accs_df.to_csv(f"./figdata/discriminator_accuracy_{pop}_{model_prefix}.csv")
+    # save
+    accs_df = pd.DataFrame(accs).T  # rows=seeds, cols=generators
+    accs_df.to_csv(PREFIX + f"figs/discriminator_accuracy_{model_prefix}.csv")
 
 def main_viz():
-    for pop in pops:
-        # also a stacked figure
-        fig, axs = plt.subplots(2, 1, figsize=(8, 12))
-        for i, model_str in enumerate(model_strs):
-            model_str = model_str.format(pop=pop)
-            model_prefix = model_str.split("N")[0].rsplit("/")[0].replace("-", "").replace("_", "")
-            accs_df = pd.read_csv(f"./figdata/discriminator_accuracy_{pop}_{model_prefix}.csv", index_col=0)
-            model_prefix = "GAN" if model_prefix == "models" else "New"
+    
+    fig, axs = plt.subplots(2, 1, figsize=(8, 12))
+    model_prefix = model_str.split("N")[0].rsplit("/")[0].replace("-", "").replace("_", "")
+    accs_df = pd.read_csv(PREFIX + f"figs/discriminator_accuracy_{model_prefix}.csv", index_col=0)
+    model_prefix = "GAN" if model_prefix == "models" else "New"
 
-            # print overall accuracy for each seed
-            # for seed, acc in accs_df["overall"].items():
-            #     print(f"Seed {seed:<2}: {acc:.4f}")
-            print(f"Overall, {pop}, {model_prefix}: {accs_df['overall'].mean():<.3f} ({accs_df['overall'].std():.3f})")
-            accs_df = accs_df.drop(columns=["overall"])
+    # print overall accuracy for each seed
+    # for seed, acc in accs_df["overall"].items():
+    #     print(f"Seed {seed:<2}: {acc:.4f}")
+    print(f"Overall, {pop}, {model_prefix}: {accs_df['overall'].mean():<.3f} ({accs_df['overall'].std():.3f})")
+    accs_df = accs_df.drop(columns=["overall"])
 
-            # plt.figure(figsize=(10, 6))
-            # sns.heatmap(accs_df, annot=True, fmt=".2f", cmap="viridis", vmin=0, vmax=1)
-            # plt.xlabel("Generator")
-            # plt.ylabel("Discriminator")
-            # plt.title(f"{pop}")
-            # plt.savefig(f"./figs/discriminator_accuracy_heatmap_{pop}_{model_prefix}.pdf", dpi=300, bbox_inches='tight')
-        
-            sns.heatmap(accs_df, annot=True, fmt=".2f", cmap="viridis", cbar=False, vmin=0, vmax=1, ax=axs[i])
-            axs[i].set_xlabel("Generator")
-            axs[i].set_ylabel("Discriminator")
-            axs[i].set_title(f"{model_prefix}")
+    # plt.figure(figsize=(10, 6))
+    # sns.heatmap(accs_df, annot=True, fmt=".2f", cmap="viridis", vmin=0, vmax=1)
+    # plt.xlabel("Generator")
+    # plt.ylabel("Discriminator")
+    # plt.title(f"{pop}")
+    # plt.savefig(f"./figs/discriminator_accuracy_heatmap_{pop}_{model_prefix}.pdf", dpi=300, bbox_inches='tight')
 
-        fig.suptitle(
-            pop,
-            y=0.93,
-            x=0.1,
-            fontsize=16,
-            bbox=dict(facecolor='white', edgecolor='black', linewidth=1)
-        )
-        fig.savefig(f"./figs/discriminator_accuracy_heatmap_{pop}.pdf", dpi=300, bbox_inches='tight')
+    sns.heatmap(accs_df, annot=True, fmt=".2f", cmap="viridis", cbar=False, vmin=0, vmax=1, ax=axs[i])
+    axs[i].set_xlabel("Generator")
+    axs[i].set_ylabel("Discriminator")
+    axs[i].set_title(f"{model_prefix}")
+
+    fig.suptitle(
+        pop,
+        y=0.93,
+        x=0.1,
+        fontsize=16,
+        bbox=dict(facecolor='white', edgecolor='black', linewidth=1)
+    )
+    fig.savefig(f"./figs/discriminator_accuracy_heatmap_{pop}.pdf", dpi=300, bbox_inches='tight')
 
 
 if __name__ == "__main__":
--- scripts/3_figs.sh
+++ scripts/3_figs.sh
@@ -1,22 +1,20 @@
-set -euo pipefail
+#set -euo pipefail
 
 # make accuracy heatmaps (for all pops)
 python compare_discs.py pred
-python compare_discs.py fig
+#python compare_discs.py fig
 
 # combined correlation experiment plot (only CEU)
-python correlation.py CEU exp-random 0
+#python correlation.py CEU exp-random 0
 
 # linear regression experiment (only CEU)
-python correlation.py CEU linreg
+#python correlation.py CEU linreg
 
-for pop in CEU CHB YRI
-do
-    # single correlation
-    python correlation.py $pop corr disc_0
-    python correlation.py $pop corr ${pop}_0_230410
+# single correlation
+#python correlation.py $pop corr disc_0
+#python correlation.py $pop corr ${pop}_0_230410
 
-    # forest features and single decision tree
-    python dtree.py $pop forest
-    python dtree.py $pop single
-done
+# forest features and single decision tree
+#python dtree.py $pop forest
+#python dtree.py $pop single
+
